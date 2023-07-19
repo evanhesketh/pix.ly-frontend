962b21aff0bc0d646c0085e9e16176379a211031
Evan Hesketh <evanhesketh@yahoo.ca>
2023-07-19 13:14:17 -0400

    migrate Edit to ts

--- src/Edit.tsx
+++ src/Edit.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, MouseEvent } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Photo from "./Photo";
 import Alert from "./Alert";
 import "./Edit.css";
+import { PhotoInterface } from "./interfaces";
+
+interface EditInterface {
+  photos: PhotoInterface[],
+  handleEdit: (fileName: String, method: String) => Promise<void>
+}
 
 /**
  * Edit a photo.
@@ -18,24 +24,21 @@ import "./Edit.css";
  * RoutesList -> Edit
  */
 
-function Edit({ photos, handleEdit }) {
+function Edit({ photos, handleEdit }: EditInterface) {
   const [error, setErrors] = useState({ error: "" });
 
   console.log("error state", error);
 
   const { id } = useParams();
-  console.log("id", id);
-  console.log("photos", photos);
-  const photo = photos.filter((p) => p.id === +id);
-  console.log("photo in Edit", photo);
+  const photo = photos.filter((p) => p.id === +id!);
   const navigate = useNavigate();
 
-  async function handleClick(evt) {
+  async function handleClick(evt: MouseEvent<HTMLButtonElement>) {
     try {
-      await handleEdit(photo[0].fileName, evt.target.id);
+      const target = evt.target as HTMLButtonElement;
+      await handleEdit(photo[0].fileName, target.id);
       navigate("/");
-    } catch (err) {
-      console.log("error in edit", err);
+    } catch (err: any) {
       setErrors({ error: err.message });
     }
   }
@@ -44,7 +47,7 @@ function Edit({ photos, handleEdit }) {
     <div className="Edit text-center">
       {error.error && <Alert error={error.error} type="alert-danger" />}
       <h4 className="Edit-heading">Choose a filter to apply to your image</h4>
-      <Photo photo={photo[0]} url={photo[0].url} />
+      <Photo photo={photo[0]} />
       <button className="btn btn-secondary" onClick={handleClick} id='bw'>
         Black and White
       </button>
